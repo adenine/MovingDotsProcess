9c8187c745138b1b49c0a13496d46ee642fdca7c
Joshua Walton <adenine@Joshuas-MacBook-Pro.local>
2020-10-10 23:26:02 -0700

    updated lerp

--- Lerp/sketch.js
+++ Lerp/sketch.js
@@ -8,6 +8,8 @@ let stopY = 300;
 let myX = startX;
 let myY = startY;
 let myLerpPercent = 0.01;
+let lerpS1 = -0.0;
+let lerpS2 = 0.5;
 
 function setup() {
   createCanvas(400, 400);
@@ -16,6 +18,11 @@ function setup() {
 function draw() {
   //background(220);
   myX = lerp(myX, stopX, myLerpPercent);
-  myY = lerp(myY, stopY, myLerpPercent*3);
+  myY = lerp(myY, stopY, myLerpPercent*(lerp(lerpS1, lerpS2, 0.1)));
   ellipse (myX, myY, 100, 100);
+}
+
+function mousePressed() {
+  stopX = mouseX;
+  stopY = mouseY;
 }
